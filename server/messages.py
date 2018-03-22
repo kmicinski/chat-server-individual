be085b1ebfc1a304a49c70c0c2e7c4d8a7c2cb08
Ziting Shen <zitingshenbmc@gmail.com>
2018-03-22 01:48:06 -0400

    modified messages.py and parser.py

--- server/messages.py
+++ server/messages.py
@@ -13,21 +13,24 @@ class JoinMessage(Message):
         self.channel = channel
 
     def render(self):
-        return "join {}".format(self.channel)
+        m = "join {}".format(self.channel)
+        return m.encode()
 
 class UpdatePasswordMessage(Message):
     def __init__(self,newpassword):
         self.newpassword = newpassword
 
     def render(self):
-        "updatepassword {}".format(self.newpassword)
+        m = "update_pw {}".format(self.newpassword)
+        return m.encode()
 
 class BlockMessage(Message):
     def __init__(self,user):
         self.blockeduser = user
 
     def render(self):
-        "block {}".format(self.blockeduser)
+        m = "block {}".format(self.blockeduser)
+        return m.encode()
 
 class BanMessage(Message):
     def __init__(self,user,channel):
@@ -35,7 +38,8 @@ class BanMessage(Message):
         self.channel = channel
 
     def render(self):
-        "ban {} {}".format(self.banneduser,self.channel)
+        m = "ban {} {}".format(self.banneduser,self.channel)
+        return m.encode()
 
 class UnbanMessage(Message):
     def __init__(self,user,channel):
@@ -43,14 +47,16 @@ class UnbanMessage(Message):
         self.channel = channel
 
     def render(self):
-        "unban {} {}".format(self.banneduser,self.channel)
+        m = "unban {} {}".format(self.banneduser,self.channel)
+        return m.encode()
 
 class GetTopicMessage(Message):
     def __init__(self,channel):
         self.channel = channel
 
     def render(self):
-        return "gettopic {}".format(self.channel)
+        m = "gettopic {}".format(self.channel)
+        return m.encode()
 
 class SetTopicMessage(Message):
     def __init__(self,channel,topic):
@@ -58,7 +64,8 @@ class SetTopicMessage(Message):
         self.topic = topic
 
     def render(self):
-        return "settopic {} {}".format(self.channel,self.topic)
+        m = "settopic {} {}".format(self.channel,self.topic)
+        return m.encode()
 
 class TopicMessage(Message):
     def __init__(self,channel,topic):
@@ -66,21 +73,17 @@ class TopicMessage(Message):
         self.topic = topic 
 
     def render(self):
-        return "topic {} {}".format(self.channel,self.topic)
-
-    def __eq__(self,other):
-        return self.channel == other.channel and self.topic == other.topic
-
-    def __ne__(self,other): 
-        return not self.__eq__(other)
-
+        m = "topic {} {}".format(self.channel,self.topic)
+        return m.encode()
+    
 class AuthenticateMessage(Message):
     def __init__(self,username,password):
         self.username = username
         self.password = password
 
     def render(self):
-        "authenticate {} {}".format(username, password)
+        m = "authenticate {} {}".format(username, password)
+        return m.encode()
 
 class RegisterMessage(Message):
     def __init__(self,username,password):
@@ -88,7 +91,8 @@ class RegisterMessage(Message):
         self.password = password
 
     def render(self):
-        "register {} {}".format(username, password)
+        m = "register {} {}".format(username, password)
+        return m.encode()
 
 class ChatMessage(Message):
     def __init__(self,user_or_channel,message):
@@ -96,14 +100,14 @@ class ChatMessage(Message):
         self.message = message
     
     def render(self):
-        m = "chat {} {} {}".format(self.user_or_channel,len(self.message),self.message)
+        m = "chat {} {}".format(self.user_or_channel,self.message)
         return m.encode()
 
 class ChatFromMessage(Message):
     """A reply from the server notifying a client that they have received a message from either a user or a channel"""
     def __init__(self,fromuser,channel,message):
         self.fromuser = fromuser
-        self.channel = message
+        self.channel = channel
         self.message = message
     
     def render(self):
@@ -116,5 +120,5 @@ class ErrorMessage(Message):
         self.message = message
     
     def render(self):
-        m = "error {} {}".format(len(self.message), self.message)
+        m = "error {}".format(self.message)
         return m.encode()
--- server/parser.py
+++ server/parser.py
@@ -10,18 +10,35 @@ class Parser():
         string = data.decode("utf-8")
         split = string.split()
         if split[0] == "authenticate":
-            return AuthenticateCommand(split[1],split[2])
+            return AuthenticateMessage(split[1],split[2])
         elif split[0] == "register":
-            return RegisterCommand(split[1],split[2])
+            return RegisterMessage(split[1],split[2])
         elif split[0] == "chat":
             msg = string.split(' ', 2)
-            return ChatCommand(msg[1],msg[2])
+            return ChatMessage(msg[1],msg[2])
+        elif split[0] == "chatfrom":
+            msg = string.split(' ', 3)
+            return ChatFromMessage(msg[1],msg[2], msg[3])
         elif split[0] == "error":
             msg = string.split(' ', 1)
-            return ErrorMessage(msg[0],msg[1])
+            return ErrorMessage(msg[1])
         elif split[0] == "join":
-            return JoinCommand(split[1])
+            return JoinMessage(split[1])
         elif split[0] == "topic":
-            return TopicMessage(split[1],string.split(2)[2])
+            msg = string.split(' ', 2)
+            return TopicMessage(msg[1],msg[2])
+        elif split[0] == "update_pw":
+            return UpdatePasswordMessage(split[1])
+        elif split[0] == "block":
+            return BlockMessage(split[1])
+        elif split[0] == "ban":
+            return BanMessage(split[1], split[2])
+        elif split[0] == "unban":
+            return UnbanMessage(split[1], split[2])
+        elif split[0] == "gettopic":
+            return GetTopicMessage(split[1])
+        elif split[0] == "settopic":
+            msg = string.split(' ', 2)
+            return SetTopicMessage(msg[1],msg[2])
         else:
             raise Exception("Could not parse packet")
